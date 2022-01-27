82b65c4cac97db0e50481eef42f6216bb048abf0
Yahya EL Ganayni <76224648+yahyaelganyni1@users.noreply.github.com>
2022-01-27 03:31:10 +0200

    Add test logic

--- spec/models/posts_spec.rb
+++ spec/models/posts_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
-RSpec.describe Posts, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'Posts', type: :request do
+  describe 'GET #index' do
+    before(:example) { get user_posts_path(1) }
+
+    it 'should have correct response status' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'should render correct template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'should include correct placeholder text' do
+      expect(response.body).to include('Posts#index')
+    end
+  end
+
+  describe 'GET #show' do
+    before(:example) { get user_post_path(1, 1) }
+
+    it 'should have correct response status' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'should render correct template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'should include correct placeholder text' do
+      expect(response.body).to include('Posts#show')
+    end
+  end
 end
--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
-RSpec.describe Users, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'Users', type: :request do
+  describe 'GET #index' do
+    before(:example) { get users_path }
+
+    it 'should have correct response status' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'should render correct template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'should include correct placeholder text' do
+      expect(response.body).to include('Users#index')
+    end
+  end
+
+  describe 'GET #show' do
+    before(:example) { get user_path(1) }
+
+    it 'should have correct response status' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'should render correct template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'should include correct placeholder text' do
+      expect(response.body).to include('Users#show')
+    end
+  end
 end
