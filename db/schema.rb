897354f2b21e54363b531684b06750942a74f6ef
Yahya EL Ganayni <76224648+yahyaelganyni1@users.noreply.github.com>
2022-02-07 03:35:46 +0200

    Implement the design

--- db/schema.rb
+++ db/schema.rb
@@ -7,10 +7,10 @@
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
-# 
+#
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_28_152317) do
+ActiveRecord::Schema.define(version: 2022_01_28_152529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2022_01_28_152317) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.integer "comments_counter", default: 0, null: false
+    t.integer "likes_counter", default: 0, null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+users = User.create([
+    {name: 'Tom', photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Breezeicons-actions-22-im-user.svg/1200px-Breezeicons-actions-22-im-user.svg.png', bio: 'Teacher from Mexico.'},
+    {name: 'Yahya', photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Breezeicons-actions-22-im-user.svg/1200px-Breezeicons-actions-22-im-user.svg.png', bio: 'full stack web develober.'},
+    {name: 'Emma', photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Breezeicons-actions-22-im-user.svg/1200px-Breezeicons-actions-22-im-user.svg.png', bio: 'Accountant a person that works with the money and accounts of a company. .'},
+    {name: 'Charlotte', photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Breezeicons-actions-22-im-user.svg/1200px-Breezeicons-actions-22-im-user.svg.png', bio: 'ctress - a person that acts in a play or a movie.'},
+    {name: 'Noah', photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Breezeicons-actions-22-im-user.svg/1200px-Breezeicons-actions-22-im-user.svg.png', bio: 'uthor - They write books or novels.'}
+    ])
+
