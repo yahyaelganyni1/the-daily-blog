af7e544aa87c277409749b91fe219ea7275f6aa6
Yahya EL Ganayni <76224648+yahyaelganyni1@users.noreply.github.com>
2022-02-12 18:19:16 +0200

    Validate fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,21 @@
-class User < ApplicationRecord
-  has_many :posts, dependent: :destroy
+class Post < ApplicationRecord
+  validates :title, presence: true,
+                    length: { maximum: 250, too_long: 'must not exceed 250 characters' }
+  validates :comments_counter, :likes_counter, numericality: { greater_than_or_equal_to: 0 }
+
+  belongs_to :user, counter_cache: :posts_counter
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
-  def recent_posts(limit = 3)
-    posts.last(limit)
+  def recent_comments(limit = 5)
+    comments.last(limit)
+  end
+
+  def liked?(user_id)
+    likes.exists?(user_id: user_id)
+  end
+
+  def update_counter(value)
+    user.update(posts_counter: value)
   end
 end
