a2e13687743d9ad38a362fdd4646385ee82ee193
Yahya EL Ganayni <76224648+yahyaelganyni1@users.noreply.github.com>
2022-02-12 18:48:25 +0200

    Fix re-render bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,12 @@
-class Post < ApplicationRecord
-  validates :title, presence: true,
-                    length: { maximum: 250, too_long: 'must not exceed 250 characters' }
-  validates :comments_counter, :likes_counter, numericality: { greater_than_or_equal_to: 0 }
+class User < ApplicationRecord
+  validates :name, presence: true
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
 
-  belongs_to :user, counter_cache: :posts_counter
+  has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
-  def recent_comments(limit = 5)
-    comments.last(limit)
-  end
-
-  def liked?(user_id)
-    likes.exists?(user_id: user_id)
-  end
-
-  def update_counter(value)
-    user.update(posts_counter: value)
+  def recent_posts(limit = 3)
+    posts.includes(:comments).last(limit)
   end
 end
